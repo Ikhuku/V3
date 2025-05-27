69bba0a489d563aa01092575b448b2473be411ff
David Ezebuilo <davidezebuilo@gmail.com>
2025-05-27 14:40:01 +0100

    created the pre-order and best seller tags

---
+++ components/BestSellTag.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BestSellTag = () => {
+  return (
+    <div className='bg-[#E6F1FC] text-sm p-1 font-semibold w-[50%] mb-2 rounded-[5px] flex items-center justify-center text-[#021236]'>
+        <p>Best seller</p>
+    </div>
+  )
+}
+
+export default BestSellTag
---
+++ components/PreOrdertag.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PreOrdertag = () => {
+  return (
+    <div className='bg-[#FE2626] text-sm p-1 font-semibold w-[50%] mb-2 rounded-[5px] flex items-center justify-center text-[#ffffff]'>
+            <p>Pre-order</p>
+        </div>
+  )
+}
+
+export default PreOrdertag
